bb4a48d252e0389d3286b7aeb965feaf847485d2
nimajo <nimajokar@hotmail.co.uk>
2023-07-10 22:32:46 +0100

    ProfileScreen : Added Changeable Profile Picture

--- compact-coach-ex/screens/ProfileScreen.js
+++ compact-coach-ex/screens/ProfileScreen.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { auth , db } from '../firebase';
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { signOut } from 'firebase/auth';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { SafeAreaView } from 'react-native';
 import { Button } from '@rneui/themed';
 import { ScrollView } from 'react-native';
-
+import * as ImagePicker from 'expo-image-picker';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 
 const ProfileScreen = () => {
   const user = auth.currentUser;
   const navigation = useNavigation();
   const [profileData, setProfileData] = useState(null);
   const [latestWeight, setLatestWeight] = useState(null);
+  const [profilePictureUrl, setProfilePictureUrl] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,7 +24,9 @@ const ProfileScreen = () => {
         const docRef = doc(db, "users", user.uid);
         const docSnap = await getDoc(docRef, { source: "server" }); // <-- fetch from server
         if (docSnap.exists()) {
-          setProfileData(docSnap.data());
+          const data = docSnap.data();
+          setProfileData(data);
+          setProfilePictureUrl(data.profilePicture);
         } else {
           console.log("No such document!");
         }
@@ -38,6 +42,42 @@ const ProfileScreen = () => {
     };
     fetchData();
   }, [user]);
+
+  const uploadProfilePicture = async (uri, uid) => {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+    const uploadTask = uploadBytesResumable(ref(storage, `profile_pictures/${uid}`), blob);
+
+    try {
+      await uploadTask;
+      const url = await getDownloadURL(ref(storage, `profile_pictures/${uid}`));
+      return url;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  };
+
+  const updateProfileWithPicture = async (uid, url) => {
+    await setDoc(doc(db, "users", uid), {
+      profilePicture: url,
+    }, { merge: true });
+  };
+
+  const onChangeProfilePicture = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      const url = await uploadProfilePicture(result.uri, user.uid);
+      await updateProfileWithPicture(user.uid, url);
+      setProfilePictureUrl(url);
+    }
+  };
 
   // Sign out function
   const signOutUser = () => {
@@ -55,8 +95,14 @@ const ProfileScreen = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <ScrollView contentContainerStyle={{flexGrow: 1}}>
-      {/** Profile Text */}
+      {/** Profile Picture and Text */}
       <View style={{alignContent:'center', marginTop:50}}>
+          <TouchableOpacity onPress={onChangeProfilePicture}>
+            <Image 
+              source={profilePictureUrl ? { uri: profilePictureUrl } : require('../images/grey-pic.jpg')} 
+              style={{ width: 100, height: 100, borderRadius: 50, alignSelf: 'center' }}
+            />
+          </TouchableOpacity>
           {profileData && (
           <Text 
           className="capitalize flex-row text-center text-xl leading-relaxed font-light "
